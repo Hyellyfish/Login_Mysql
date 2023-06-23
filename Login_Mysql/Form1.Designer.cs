440818d43259465fb8269f0930301f8994654298
KYD <yd512800@vs21.kr>
2023-06-23 10:15:09 +0900

    초기 커밋.

---
+++ Login_Mysql/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace Login_Mysql
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.name = new System.Windows.Forms.TextBox();
+            this.iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.password = new System.Windows.Forms.TextBox();
+            this.iconPictureBox2 = new FontAwesome.Sharp.IconPictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.exit_btn = new System.Windows.Forms.Button();
+            this.login_btn = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.name);
+            this.panel1.Controls.Add(this.iconPictureBox1);
+            this.panel1.Location = new System.Drawing.Point(33, 196);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(224, 43);
+            this.panel1.TabIndex = 1;
+            // 
+            // name
+            // 
+            this.name.BackColor = System.Drawing.Color.MediumPurple;
+            this.name.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.name.Font = new System.Drawing.Font("한컴 고딕", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.name.ForeColor = System.Drawing.SystemColors.Window;
+            this.name.Location = new System.Drawing.Point(40, 3);
+            this.name.Multiline = true;
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(184, 38);
+            this.name.TabIndex = 2;
+            this.name.Text = "username";
+            // 
+            // iconPictureBox1
+            // 
+            this.iconPictureBox1.BackColor = System.Drawing.Color.MediumPurple;
+            this.iconPictureBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.UserAlt;
+            this.iconPictureBox1.IconColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox1.IconSize = 38;
+            this.iconPictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.iconPictureBox1.Name = "iconPictureBox1";
+            this.iconPictureBox1.Size = new System.Drawing.Size(38, 38);
+            this.iconPictureBox1.TabIndex = 0;
+            this.iconPictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.password);
+            this.panel2.Controls.Add(this.iconPictureBox2);
+            this.panel2.Location = new System.Drawing.Point(33, 254);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(224, 43);
+            this.panel2.TabIndex = 3;
+            // 
+            // password
+            // 
+            this.password.BackColor = System.Drawing.Color.MediumPurple;
+            this.password.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.password.Font = new System.Drawing.Font("한컴 고딕", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.password.ForeColor = System.Drawing.SystemColors.Window;
+            this.password.Location = new System.Drawing.Point(40, 3);
+            this.password.Multiline = true;
+            this.password.Name = "password";
+            this.password.PasswordChar = '⦁';
+            this.password.Size = new System.Drawing.Size(184, 37);
+            this.password.TabIndex = 3;
+            this.password.Text = "password";
+            // 
+            // iconPictureBox2
+            // 
+            this.iconPictureBox2.BackColor = System.Drawing.Color.MediumPurple;
+            this.iconPictureBox2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.iconPictureBox2.IconChar = FontAwesome.Sharp.IconChar.Lock;
+            this.iconPictureBox2.IconColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.iconPictureBox2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox2.IconSize = 38;
+            this.iconPictureBox2.Location = new System.Drawing.Point(3, 3);
+            this.iconPictureBox2.Name = "iconPictureBox2";
+            this.iconPictureBox2.Size = new System.Drawing.Size(38, 38);
+            this.iconPictureBox2.TabIndex = 0;
+            this.iconPictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::Login_Mysql.Properties.Resources.icon1;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(99, 71);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // exit_btn
+            // 
+            this.exit_btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.exit_btn.FlatAppearance.BorderSize = 0;
+            this.exit_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exit_btn.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.exit_btn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.exit_btn.Location = new System.Drawing.Point(255, 12);
+            this.exit_btn.Name = "exit_btn";
+            this.exit_btn.Size = new System.Drawing.Size(22, 23);
+            this.exit_btn.TabIndex = 4;
+            this.exit_btn.Text = "X";
+            this.exit_btn.UseVisualStyleBackColor = true;
+            this.exit_btn.Click += new System.EventHandler(this.exit_btn_Click);
+            // 
+            // login_btn
+            // 
+            this.login_btn.BackColor = System.Drawing.Color.MediumPurple;
+            this.login_btn.FlatAppearance.BorderSize = 0;
+            this.login_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.login_btn.Font = new System.Drawing.Font("한컴 고딕", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.login_btn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.login_btn.Location = new System.Drawing.Point(104, 351);
+            this.login_btn.Name = "login_btn";
+            this.login_btn.Size = new System.Drawing.Size(79, 31);
+            this.login_btn.TabIndex = 5;
+            this.login_btn.Text = "Log in";
+            this.login_btn.UseVisualStyleBackColor = false;
+            this.login_btn.Click += new System.EventHandler(this.login_btn_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.checkBox1.Location = new System.Drawing.Point(33, 314);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(108, 16);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Remember me";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.LightSalmon;
+            this.label1.Location = new System.Drawing.Point(150, 315);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Forgot Password?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.ClientSize = new System.Drawing.Size(289, 435);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.login_btn);
+            this.Controls.Add(this.exit_btn);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "    ";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox name;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox2;
+        private System.Windows.Forms.Button exit_btn;
+        private System.Windows.Forms.Button login_btn;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox password;
+    }
+}
+
---
+++ Login_Mysql/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Login_Mysql
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exit_btn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void login_btn_Click(object sender, EventArgs e)
+        {
+            // SQL 서버 연결
+            string cs = "Server=192.168.0.53;port=3306;Database=test_db;Uid=root;Pwd=1111;";
+            var con = new MySqlConnection(cs);
+            string login_status = "n";
+
+            try {
+                con.Open();
+                string stm = "select * from test1_hr where userid =@Userid AND userpw =@Userpw";
+                var cmd = new MySqlCommand(stm, con);
+
+                Console.WriteLine("입력 ID : "+ name.Text);
+                Console.WriteLine("입력 PW : "+ password.Text);
+                cmd.Parameters.AddWithValue("@Userid", name.Text); // name : username 입력란
+                cmd.Parameters.AddWithValue("@Userpw", password.Text); // password : userpw 입력란
+                
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    login_status = "y";
+                }
+                rdr.Close();
+
+                if (login_status.Equals("y"))
+                { // 로그인 성공 시
+                    Console.WriteLine("로그인 성공");
+                    main_form();
+                }
+                else // 로그인 실패 시
+                {
+                    Console.WriteLine("로그인 실패");
+                }
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+            con.Close();
+        }
+
+        private void main_form() {
+            this.Hide();
+            main frm = new main();
+            frm.ShowDialog();
+            this.Close();
+        }
+    }
+}
